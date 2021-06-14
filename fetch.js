9bbf8ab57c39a52fcd850bbd1c288aefe734cb50
wifabio78 <85130252+wifabio78@users.noreply.github.com>
2021-06-14 13:26:42 -0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -92,7 +92,9 @@ function submitPut(){
 
 function submitDelete(){
     const form = document.forms['deleteForm'];  
-    const id = form["time"].value;  
+    const id = form["id"].value; 
+    const novocra = form["time"].value;
+    const novotype = form["type"].value;
     callFetchWithDelete(id);
     return false; // Evitar o reload da tela.
 }
